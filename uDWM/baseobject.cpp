4141b829324fcaf11992da5e41f7fd90d7559ec4
Dr1111 <doctorwhy1111@gmail.com>
2025-07-03 17:39:43 +0700

    add

---
+++ uDWM/baseobject.cpp
@@ -0,0 +1,31 @@
+#include "C:/Users/Admin/source/repos/uDWM/uDWM/baseobject.h"
+
+
+CBaseObject::CBaseObject(CBaseObject&)
+{
+}
+
+CBaseObject::CBaseObject()
+{
+}
+
+CBaseObject::~CBaseObject()
+{
+	// hi
+}
+
+ULONG CBaseObject::AddRef()
+{
+	int result;
+	result = this->m_cRef + 1;
+	this->m_cRef = result;
+	return result;
+}
+
+ULONG CBaseObject::Release()
+{
+	bool v1;
+	v1 = this->m_cRef-- == 1;
+	m_cRef = this->m_cRef;
+	return m_cRef;
+}
---
+++ uDWM/bitmapsource.cpp
@@ -0,0 +1,62 @@
+#include "C:\Users\Admin\source\repos\uDWM\uDWM\bitmapsource.h"
+#include "C:\Users\Admin\source\repos\uDWM\uDWM\framework.h"
+
+HRESULT CBitmapSource::Create(const MIL_CHANNEL__*, wchar_t*, CBitmapSource**)
+{
+	return E_NOTIMPL;
+}
+
+HRESULT CBitmapSource::Create(UINT, UINT, UINT, UINT, void*, const MIL_CHANNEL__*, CBitmapSource**)
+{
+	return E_NOTIMPL;
+}
+
+HRESULT CBitmapSource::Create(CBitmapSource*, tagRECT&, _MARGINS*, CBitmapSource**)
+{
+	return E_NOTIMPL;
+}
+
+HRESULT CBitmapSource::Create(void*, unsigned long, const MIL_CHANNEL__*, CBitmapSource**)
+{
+	return E_NOTIMPL;
+}
+
+HRESULT CBitmapSource::Create(IWICBitmap*, _MARGINS*, const MIL_CHANNEL__*, CBitmapSource**)
+{
+	return E_NOTIMPL;
+}
+
+HRESULT CBitmapSource::Create(HICON__*, const MIL_CHANNEL__*, CBitmapSource**)
+{
+	return E_NOTIMPL;
+}
+
+HRESULT CBitmapSource::Create(HINSTANCE__*, void*, const int, const MIL_CHANNEL__*, CBitmapSource**)
+{
+	return E_NOTIMPL;
+}
+
+HRESULT CBitmapSource::Create(const MIL_CHANNEL__*, CBitmapSource**)
+{
+	return E_NOTIMPL;
+}
+
+UINT CBitmapSource::GetWidth()
+{
+	return 0;
+}
+
+UINT CBitmapSource::GetHeight()
+{
+	return 0;
+}
+
+tagSIZE& CBitmapSource::GetSize()
+{
+	// TODO: insert return statement here
+}
+
+tagRECT& CBitmapSource::GetSourceClip()
+{
+	// TODO: insert return statement here
+}
